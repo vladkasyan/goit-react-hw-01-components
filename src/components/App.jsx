0cc3709e7a816b775e0521ba09040159d77b9016
vladkasyan <voadkasyan.2006@gmail.com>
2023-08-22 12:23:59 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import user from '../assets/user.json';
+import data from '../assets/data.json';
+import friends from '../assets/friends.json';
+import transactions from '../assets/transactions.json';
+
+import { Profile } from "./Profile/Profile";
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { Container, Wrapper } from './App.module';
+
+
 export const App = () => {
   return (
     <Container>
