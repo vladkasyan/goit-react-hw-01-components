bc73fe2b55d002ac779c0bdb578e2194e2bb7ce2
vladkasyan <voadkasyan.2006@gmail.com>
2023-08-22 12:38:33 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from '../assets/user.json';
-import data from '../assets/data.json';
-import friends from '../assets/friends.json';
-import transactions from '../assets/transactions.json';
+import user from '../components/data/user.json';
+import data from '../components/data/data.json';
+import friends from '../components/data/friends.json';
+import transactions from '../components/data/transactions.json';
 
 import { Profile } from "./Profile/Profile";
 import { Statistics } from './Statistics/Statistics';
